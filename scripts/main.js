bdb26b8c2bda5380f6d390eaa2234cda4d5d57dd
NagamalleswaraRao <ngannavarapu@rythmos.com>
2017-02-17 20:20:15 +0530

    written dropdown functionality...

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,57 @@
 var app = angular.module("app", []);
-
 app.controller("controller", function($scope) {
-
-    $scope.items = [{
-        name: "Chicken",
-        options: [
-            "Drumstick",
-            "Thigh",
-            "Wing"
-        ]
-    }, {
-        name: "Meat",
-        options: [
-            "Lamb",
-            "Goat",
-        ]
-    }, {
-        name: "Fish"
-    }];
-
+    $scope.countries = {
+        'India': {
+            'Maharashtra': ['Pune', 'Mumbai', 'Nagpur', 'Akola'],
+            'Madhya Pradesh': ['Indore', 'Bhopal', 'Jabalpur'],
+            'Rajasthan': ['Jaipur', 'Ajmer', 'Jodhpur']
+        },
+        'USA': {
+            'Alabama': ['Montgomery', 'Birmingham'],
+            'California': ['Sacramento', 'Fremont'],
+            'Illinois': ['Springfield', 'Chicago']
+        },
+        'Australia': {
+            'New South Wales': ['Sydney'],
+            'Victoria': ['Melbourne']
+        }
+    };
+    $scope.GetSelectedCountry = function() {
+        $scope.strCountry = document.getElementById("country").value;
+    };
+    $scope.GetSelectedState = function() {
+        $scope.strState = document.getElementById("state").value;
+    };
+    // $scope.items = [{
+    //     name: "Chicken",
+    //     options: [
+    //         "Drumstick",
+    //         "Thigh",
+    //         "Wing"
+    //     ],
+    //     description: [
+    //         "hi",
+    //         "hello"
+    //     ]
+    // }, {
+    //     name: "Meat",
+    //     options: [
+    //         "Lamb",
+    //         "Goat",
+    //     ],
+    //     description: [
+    //         "h r u ",
+    //         "wt abt you "
+    //     ]
+    // }, {
+    //     name: "Fish",
+    //     options: [
+    //         "Pulasa",
+    //         "royya",
+    //     ],
+    //     description: [
+    //         "who r u",
+    //         "i am malli"
+    //     ]
+    // }];
 });
